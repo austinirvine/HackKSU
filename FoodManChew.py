581c00f784c41395fd1eb5043704ba8300844ed2
Andy Monroe <jayhawker805@gmail.com>
2016-04-30 07:09:00 -0500

    Add basic selection to FoodManChew.py

--- FoodManChew.py
+++ FoodManChew.py
@@ -1,1 +1,10 @@
 
+print("Choose a food genre:")
+print("Asian")
+print("Cookies")
+print("Pizza")
+print("Other")
+choice = raw_input("\nSelection: ")
+
+if choice.upper() == "ASIAN":
+    execfile("HunamScraper.py")
