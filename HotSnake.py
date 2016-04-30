ef3f528c3e915e5a05c79e404f8a0ade7bb4aede
Andy Monroe <jayhawker805@gmail.com>
2016-04-30 15:59:32 -0500

    Finish move and grow functions for snake

--- HotSnake.py
+++ HotSnake.py
@@ -1,14 +1,39 @@
 from __future__ import print_function
 
-#def grow_snake(snek):
+def grow_snake(snk, dir):
+    if dir == 0:
+        snk.insert(0, [snk[0][0]-1, snk[0][1]])
+    elif dir == 1:
+        snk.insert(0, [snk[0][0], snk[0][1]+1])
+    elif dir == 2:
+        snk.insert(0, [snk[0][0]+1, snk[0][1]])
+    elif dir == 3:
+        snk.insert(0, [snk[0][0], snk[0][1]-1])
 
-def move_snake(snk):
-    snk.insert(0, [snk[0][0], snk[0][1]+1])
+    return(snk)
+
+def move_snake(snk, dir):
+    if dir == 0:
+        snk.insert(0, [snk[0][0]-1, snk[0][1]])
+    elif dir == 1:
+        snk.insert(0, [snk[0][0], snk[0][1]+1])
+    elif dir == 2:
+        snk.insert(0, [snk[0][0]+1, snk[0][1]])
+    elif dir == 3:
+        snk.insert(0, [snk[0][0], snk[0][1]-1])
+        
     del snk[len(snk) - 1]
 
     return(snk)
 
 def update_field(fld, size, snk):
+    for x in range(0, size):
+        for y in range(0, size):
+            if x == 0 or y == 0 or x == size - 1 or y == size - 1:
+                field[x][y] = "#"
+            else:
+                field[x][y] = " "
+
     for x in range(0, len(snk)):
         fld[snk[x][0]][snk[x][1]] = "X"
         print()
@@ -25,6 +50,7 @@ def draw_field(fld):
 field = []
 snake = []
 size = 20
+direction = 0
 
 #create initial snake
 snake.append([int(size / 2), int(size / 2)])
@@ -45,12 +71,12 @@ for x in range(0, size):
 field = update_field(field, size, snake)
 draw_field(field)
 
-snake = move_snake(snake)
+snake = move_snake(snake, 0)
 
 field = update_field(field, size, snake)
 draw_field(field)
 
-snake = move_snake(snake)
+snake = grow_snake(snake, 0)
 
 field = update_field(field, size, snake)
 draw_field(field)
