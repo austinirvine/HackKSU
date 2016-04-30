59fab47b87fba7e467b4a65960860a7c9370b747
Andy Monroe <jayhawker805@gmail.com>
2016-04-30 14:25:49 -0500

    Make field size easily adjustable

--- HotSnake.py
+++ HotSnake.py
@@ -1,18 +1,19 @@
 from __future__ import print_function
 
 field = []
+size = 20
 
-for x in range(0, 10):
+for x in range(0, size):
     field.append([])
 
-for x in range(0, 10):
-    for y in range(0, 10):
-        if x == 0 or y == 0 or x == 9 or y == 9:
+for x in range(0, size):
+    for y in range(0, size):
+        if x == 0 or y == 0 or x == size - 1 or y == size - 1:
             field[x].append('#')
         else:
             field[x].append(' ')
 
-for x in range(0, 10):
-    for y in range(0, 10):
+for x in range(0, size):
+    for y in range(0, size):
         print(field[x][y], end='')
     print('\n', end='')
