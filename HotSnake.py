e4723c89c2bea15cea24b7cf3771a8613ff19369
Andy Monroe <jayhawker805@gmail.com>
2016-04-30 15:53:07 -0500

    Snake can move manually now

--- HotSnake.py
+++ HotSnake.py
@@ -2,35 +2,55 @@ from __future__ import print_function
 
 #def grow_snake(snek):
 
-def move_snake(snek, dir):
-    snek.insert(0, [snek+1, snek])
-    del snek[len(snek) - 1]
+def move_snake(snk):
+    snk.insert(0, [snk[0][0], snk[0][1]+1])
+    del snk[len(snk) - 1]
 
-    return(snek)
+    return(snk)
 
 def update_field(fld, size, snk):
-    for x in range(0, size):
-        for y in range(0, size):
-            if x == 0 or y == 0 or x == size - 1 or y == size - 1:
-                field[x].append("#")
-            else:
-                field[x].append(" ")
-
     for x in range(0, len(snk)):
-        field[snk[x][0]][snk[x][1]] = "X"
+        fld[snk[x][0]][snk[x][1]] = "X"
+        print()
 
+    return(fld)
+
+def draw_field(fld):
+    for x in fld:
+        for y in x:
+            print(y, end='')
+        print('\n', end='')
+
+#variables
 field = []
 snake = []
 size = 20
 
+#create initial snake
+snake.append([int(size / 2), int(size / 2)])
+
+#create initial field
 for x in range(0, size):
     field.append([])
 
-snake.append([int(size / 2), int(size / 2)])
-
-field = update_field(field, size, snake)
-
 for x in range(0, size):
     for y in range(0, size):
-        print(str(field[x][y]), end='')
-    print('\n', end='')
+        if x == 0 or y == 0 or x == size - 1 or y == size - 1:
+            field[x].append("#")
+        else:
+            field[x].append(" ")
+
+
+
+field = update_field(field, size, snake)
+draw_field(field)
+
+snake = move_snake(snake)
+
+field = update_field(field, size, snake)
+draw_field(field)
+
+snake = move_snake(snake)
+
+field = update_field(field, size, snake)
+draw_field(field)
