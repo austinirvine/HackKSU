97f9e65f55291d9546d5a0afdffd6a7c0ef5372b
Andy Monroe <jayhawker805@gmail.com>
2016-04-30 00:49:37 -0500

    Filter out newlines from fav hero list

--- FoodManChew.py
+++ FoodManChew.py
@@ -1,13 +1,19 @@
 import urllib2
 from bs4 import BeautifulSoup
 
+def remove_newlines(temp):
+        if temp.find("u'\n'") == -1:
+            return False
+        else:
+            return True
+
 soup = BeautifulSoup(urllib2.urlopen('http://people.cis.ksu.edu/~hahnd11/First%20Site/').read())
 
 page_title = soup.title.string
 
 print(page_title)
 
-fav_heroes = soup.body.ol.contents
+fav_heroes = filter(remove_newlines, soup.body.ol.contents)
 
 for x in range(0,len(fav_heroes)):
     print(fav_heroes[x])
