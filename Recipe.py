255b3870ce9525f161f8b21cf5c0e4811f6e89c9
Andy Monroe <jayhawker805@gmail.com>
2016-04-30 11:43:30 -0500

    Add Recipe class with constructor

---
+++ Recipe.py
@@ -0,0 +1,7 @@
+class Recipe:
+    name = "";
+    ingredients = []
+    def __init__(self, rec_name, num_ing):
+        self.name = rec_name
+        for x in range(0, num_ing)
+            self.ingredients.append([])
