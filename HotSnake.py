4ff4c4099c521509636561b65f23c1dad04fa522
Andy Monroe <jayhawker805@gmail.com>
2016-04-30 16:30:13 -0500

    Enable user to control snake movement

--- HotSnake.py
+++ HotSnake.py
@@ -1,27 +1,27 @@
 from __future__ import print_function
 
 def grow_snake(snk, dir):
-    if dir == 0:
+    if dir == 1:
         snk.insert(0, [snk[0][0]-1, snk[0][1]])
-    elif dir == 1:
-        snk.insert(0, [snk[0][0], snk[0][1]+1])
     elif dir == 2:
-        snk.insert(0, [snk[0][0]+1, snk[0][1]])
+        snk.insert(0, [snk[0][0], snk[0][1]+1])
     elif dir == 3:
+        snk.insert(0, [snk[0][0]+1, snk[0][1]])
+    elif dir == 4:
         snk.insert(0, [snk[0][0], snk[0][1]-1])
 
     return(snk)
 
 def move_snake(snk, dir):
-    if dir == 0:
+    if dir == 1:
         snk.insert(0, [snk[0][0]-1, snk[0][1]])
-    elif dir == 1:
-        snk.insert(0, [snk[0][0], snk[0][1]+1])
     elif dir == 2:
-        snk.insert(0, [snk[0][0]+1, snk[0][1]])
+        snk.insert(0, [snk[0][0], snk[0][1]+1])
     elif dir == 3:
+        snk.insert(0, [snk[0][0]+1, snk[0][1]])
+    elif dir == 4:
         snk.insert(0, [snk[0][0], snk[0][1]-1])
-        
+
     del snk[len(snk) - 1]
 
     return(snk)
@@ -36,7 +36,6 @@ def update_field(fld, size, snk):
 
     for x in range(0, len(snk)):
         fld[snk[x][0]][snk[x][1]] = "X"
-        print()
 
     return(fld)
 
@@ -46,11 +45,24 @@ def draw_field(fld):
             print(y, end='')
         print('\n', end='')
 
+def is_game_over(fld, snk):
+    for x in range(0, size):
+        for y in range(0, size):
+            if (x == 0 or y == 0 or x == size - 1 or y == size - 1) and fld[x][y] != "#":
+                return True
+
+    for x in range(0, len(snk)):
+        for y in range(0, len(snk)):
+            if [[snk[x][0]],[snk[x][1]]] == [[snk[y][0]],[snk[y][1]]] and x != y:
+                return True
+
+    return False
+
 #variables
 field = []
 snake = []
 size = 20
-direction = 0
+direction = 3
 
 #create initial snake
 snake.append([int(size / 2), int(size / 2)])
@@ -66,17 +78,11 @@ for x in range(0, size):
         else:
             field[x].append(" ")
 
+while is_game_over(field, snake) == False:
+    field = update_field(field, size, snake)
+    draw_field(field)
 
+    if is_game_over(field, snake) == False:
+        direction = int(input(": "))
 
-field = update_field(field, size, snake)
-draw_field(field)
-
-snake = move_snake(snake, 0)
-
-field = update_field(field, size, snake)
-draw_field(field)
-
-snake = grow_snake(snake, 0)
-
-field = update_field(field, size, snake)
-draw_field(field)
+        snake = grow_snake(snake, direction)
