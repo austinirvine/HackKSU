c0704ad8537fdeac4b061f7cc7908f998a230603
Andy Monroe <jayhawker805@gmail.com>
2016-04-30 14:58:14 -0500

    Commit to save progress

--- HotSnake.py
+++ HotSnake.py
@@ -1,19 +1,36 @@
 from __future__ import print_function
 
+#def grow_snake(snek):
+
+def move_snake(snek, dir):
+    snek.insert(0, [snek+1, snek])
+    del snek[len(snek) - 1]
+
+    return(snek)
+
+def update_field(fld, size, snk):
+    for x in range(0, size):
+        for y in range(0, size):
+            if x == 0 or y == 0 or x == size - 1 or y == size - 1:
+                field[x].append("#")
+            else:
+                field[x].append(" ")
+
+    for x in range(0, len(snk)):
+        field[snk[x][0]][snk[x][1]] = "X"
+
 field = []
+snake = []
 size = 20
 
 for x in range(0, size):
     field.append([])
 
-for x in range(0, size):
-    for y in range(0, size):
-        if x == 0 or y == 0 or x == size - 1 or y == size - 1:
-            field[x].append('#')
-        else:
-            field[x].append(' ')
+snake.append([int(size / 2), int(size / 2)])
+
+field = update_field(field, size, snake)
 
 for x in range(0, size):
     for y in range(0, size):
-        print(field[x][y], end='')
+        print(str(field[x][y]), end='')
     print('\n', end='')
