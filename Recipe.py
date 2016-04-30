9c36bb38e7c38bfd951a5f6d0b24eef868253388
Andy Monroe <jayhawker805@gmail.com>
2016-04-30 11:52:01 -0500

    Add member methods for Recipe class

--- Recipe.py
+++ Recipe.py
@@ -1,7 +1,19 @@
 class Recipe:
     name = "";
     ingredients = []
-    def __init__(self, rec_name, num_ing):
+    def __init__(self, rec_name):
         self.name = rec_name
-        for x in range(0, num_ing)
-            self.ingredients.append([])
+
+    def add_ingredient(self, ing_name, ing_qnty):
+        self.ingredients.append([])
+        self.ingredients[len(ingredients) - 1].append(ing_name)
+        self.ingredients[len(ingredients) - 1].append(ing_qnty)
+
+    def return_recipe_name(self):
+        return(self.name)
+
+    def return_ingredient_name(self, index):
+        return(self.ingredients[index][0])
+
+    def return_ingredient_qnty(self, index):
+        return(self.ingredients[index][1])
